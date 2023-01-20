4c7bfa422bfff1752a5c99ec8f45a98cef90478c
interpreterK <rhpidfyre@tuta.io>
2023-01-19 20:09:08 -0500

    Tweaks and more animation to objects

--- src/Modules/UI-Vector.js
+++ src/Modules/UI-Vector.js
@@ -1,11 +1,18 @@
-import * as Lib from './Library.js'
+import * as Shared from './Shared.js'
 
 export let win_Offset = 3 // Usually a good offset for browser true size is 3
 
+/**
+ * get_POS (OBJ, C_DATA)
+ * @function
+ * @param {HTMLElement} OBJ - An HTML Element, should always be a div. 
+ * @returns 
+ */
 export function get_POS(OBJ) {
+    const BoundingRect = OBJ.getBoundingClientRect()
 	return {
-		X: OBJ.offsetLeft+OBJ.offsetWidth/2,
-		Y: OBJ.offsetRight+OBJ.offsetHeight/2
+		X: BoundingRect.left,
+		Y: BoundingRect.top
 	}
 }
 
@@ -18,11 +25,11 @@ export function get_POS(OBJ) {
  */
 export function set_POS(OBJ, C_DATA) {
 	const P = get_POS(OBJ)
-	C_DATA.NEW_X = Lib.s_Circuit(C_DATA.NEW_X, P.X)
-	C_DATA.NEW_Y = Lib.s_Circuit(C_DATA.NEW_Y, P.Y)
+	C_DATA.NEW_X = Shared.s_Circuit(C_DATA.NEW_X, P.X)
+	C_DATA.NEW_Y = Shared.s_Circuit(C_DATA.NEW_Y, P.Y)
 
-	OBJ.style.left = P.X+C_DATA.NEW_X+'px'
-	OBJ.style.top  = P.Y+C_DATA.NEW_Y+'px'
+	OBJ.style.left = C_DATA.NEW_X+'px'
+	OBJ.style.top  = C_DATA.NEW_Y+'px'
 	OBJ.style.position = 'absolute'
 
 	return {X: C_DATA.NEW_X, Y: C_DATA.NEW_Y}
@@ -36,7 +43,7 @@ export function set_POS(OBJ, C_DATA) {
  * @returns 
  */
 export function set_ROT(OBJ, R_DATA) {
-	R_DATA.RAD = Lib.s_Circuit(R_DATA.RAD, '0')
+	R_DATA.RAD = Shared.s_Circuit(R_DATA.RAD, '0')
 	OBJ.style.transform = 'rotate('+R_DATA.RAD+'rad)'
 
 	return {R: R_DATA.RAD}
@@ -51,11 +58,11 @@ export function set_ROT(OBJ, R_DATA) {
 */
 export function set_POS_Rel(OBJ, C_DATA) {
 	const P = get_POS(OBJ)
-	C_DATA.NEW_X = Lib.s_Circuit(C_DATA.NEW_X, P.X)
-	C_DATA.NEW_Y = Lib.s_Circuit(C_DATA.NEW_Y, P.Y)
+	C_DATA.NEW_X = Shared.s_Circuit(C_DATA.NEW_X, P.X)
+	C_DATA.NEW_Y = Shared.s_Circuit(C_DATA.NEW_Y, P.Y)
 
-	const w_xM = window.innerWidth-win_Offset+(P.X-C_DATA.NEW_X)
-	const w_yM = window.innerHeight-win_Offset+(P.Y-C_DATA.NEW_Y)
+	const w_xM = window.innerWidth-win_Offset-C_DATA.NEW_X
+	const w_yM = window.innerHeight-win_Offset-C_DATA.NEW_Y
 	OBJ.style.left = w_xM+'px'
 	OBJ.style.top  = w_yM+'px'
 	OBJ.style.position = 'absolute'
---
+++ src/UI.js
@@ -0,0 +1,90 @@
+import * as UI_V from './Modules/UI-Vector.js'
+import * as cMath from './Modules/cMath.js'
+
+const Object_1 = document.getElementById("Object-1")
+
+// UI Element Resizing
+const BottomBar   = document.getElementById("Bottom-Bar")
+const BottomBar_2 = document.getElementById("Bottom-Bar-2")
+const Bottom_Rect = document.getElementById("Bottom-Rect")
+const Bottom_Rect_2 = document.getElementById("Bottom-Rect-2")
+
+window.onresize = () => {
+	const w_x = window.innerWidth-UI_V.win_Offset
+	const w_y = window.innerHeight-UI_V.win_Offset
+
+	BottomBar.style.width = w_x+'px'
+    BottomBar_2.style.width = w_x+'px'
+
+    // These do not require UI_V.set_POS_Rel.
+    UI_V.set_POS(Bottom_Rect, {
+        NEW_X: w_x/2.03,
+        NEW_Y: w_y-BottomBar.offsetHeight*2.2
+    })
+    UI_V.set_POS(Bottom_Rect_2, {
+        NEW_X: w_x/2.07,
+        NEW_Y: w_y-Bottom_Rect_2.offsetHeight*1.5
+    })
+    UI_V.set_POS(BottomBar_2, {
+        NEW_Y: w_y-100
+    })
+}
+
+// Pre-Init Sizing
+const w_x = window.innerWidth-UI_V.win_Offset
+const w_y = window.innerHeight-UI_V.win_Offset
+
+BottomBar.style.width = w_x+'px'
+BottomBar_2.style.width = w_x+'px'
+
+UI_V.set_POS(Bottom_Rect, {
+	NEW_X: w_x/2.03,
+	NEW_Y: w_y-BottomBar.offsetHeight*2.2
+})
+UI_V.set_POS(Bottom_Rect_2, {
+	NEW_X: w_x/2.07,
+	NEW_Y: w_y-Bottom_Rect_2.offsetHeight*1.5
+})
+UI_V.set_POS(Object_1, {
+	NEW_X: w_x,
+	NEW_Y: w_y
+})
+UI_V.set_POS(BottomBar_2, {
+    NEW_Y: w_y-100
+})
+// ---
+
+const lerp = cMath.lerp
+const rad = cMath.rad
+
+const P_O1 = UI_V.get_POS(Object_1)
+let delta = 0
+
+function UI_FPS() {
+	setTimeout(() => {
+		delta+=1
+		UI_V.set_POS_Rel(Object_1, {
+			NEW_X: lerp(P_O1.X-900, 30*Math.cos(delta/150), .5),
+			NEW_Y: lerp(P_O1.Y-800, (30/2)*Math.sin(delta/150), .5)
+		})
+		UI_V.set_ROT(Object_1, {
+			RAD: rad(5*Math.cos(delta/200))
+		})
+		UI_V.set_ROT(BottomBar, {
+			RAD: rad(.5*Math.cos(delta/80))
+		})
+		UI_V.set_ROT(BottomBar_2, {
+			RAD: rad(.5*Math.sin(delta/80))
+		})
+		UI_V.set_ROT(Bottom_Rect, {
+			RAD: rad(-delta/4)
+		})
+        UI_V.set_ROT(Bottom_Rect_2, {
+			RAD: rad(delta/4)
+		})
+
+		UI_FPS()
+	}, 1)
+}
+
+UI_FPS()
