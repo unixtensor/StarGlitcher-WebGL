b912e4cd23536506c29d867eae019a1df9999572
interpreterK <rhpidfyre@tuta.io>
2023-02-01 08:18:14 -0500

    Deprecate threeShared.js at the engine level

--- src/modules/three/threeShared.js
+++
@@ -1,9 +0,0 @@
-import { Vector3 } from "three"
-
-export function lookVector(Object, Distance) {
-	return new Vector3(0,0,-Distance).applyQuaternion(Object.quaternion).add(Object.position)
-}
-
-export function rightVector(Object, Distance) {
-	return new Vector3(-Distance,0,0).applyQuaternion(Object.quaternion).add(Object.position)
-}
