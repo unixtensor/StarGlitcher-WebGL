275d700a14f34b350f9ecc596cec1db2440c3302
interpreterK <rhpidfyre@tuta.io>
2023-02-20 12:20:28 -0500

    npm update + Wings

--- src/modules/three/Wings.js
+++ src/modules/three/Wings.js
@@ -5,45 +5,22 @@ import { Union, CharacterMesh } from "./rhpidEngine/rE_Character"
 const GLTF_Loader = new GLTFLoader()
 
 export const WingAssets = {
-	Left: [],
-	Right: [],
-	Union: {
-		RingC0: null,
-		WingLC01: null, WingLC02: null, WingLC03: null,
-		WingRC01: null, WingRC02: null, WingRC02: null
-	},
-    Ring: null
-}
-
-export class Wings {
-	constructor() {}
-
-	async GlitcherWings() {
-		const DEF_ModeColor = 0xff0000
-		WingAssets.Ring = await CreateRing(DEF_ModeColor)
-		WingAssets.Left.push(CreateWing(DEF_ModeColor))
-	    WingAssets.Left.push(CreateWing(DEF_ModeColor))
-	    WingAssets.Left.push(CreateWing(DEF_ModeColor))
-	    WingAssets.Right.push(CreateWing(DEF_ModeColor, true))
-	    WingAssets.Right.push(CreateWing(DEF_ModeColor, true))
-	    WingAssets.Right.push(CreateWing(DEF_ModeColor, true))
-	
-	    WingAssets.Union.RingC0 = new Union(CharacterMesh.Torso, WingAssets.Ring)
-	}
-
-	Wing_Unions_update() {
-		WingAssets.Union.RingC0.C0()
-	}
+    Left: [],
+    Right: [],
+    Ring: null,
+    Union: {
+        RingC0: null,
+        WingLC01: null, WingLC02: null, WingLC03: null,
+        WingRC01: null, WingRC02: null, WingRC02: null
+    },
 }
 
 async function GLTF(GLTF_FILE) {
-    const Asset = new Promise((resolve, _) => GLTF_Loader.load(GLTF_FILE, (gltf_obj) => resolve(gltf_obj)))
-    Asset.catch((reason) => console.error(reason))
-    return Asset
+    return new Promise((resolve, _) => GLTF_Loader.load(GLTF_FILE, (gltf_obj) => resolve(gltf_obj))).catch((reason) => console.error(reason))
 }
 
 async function CreateWing(Color = 0xffffff, LeftSided) {
-    const WingGLTF = await GLTF('/public/3D/Wing.gltf')
+    const WingGLTF = await GLTF('/3D/Wing.gltf')
     const Side = LeftSided && WingAssets.Left || WingAssets.Right
     let WingObject = null
 
@@ -52,19 +29,15 @@ async function CreateWing(Color = 0xffffff, LeftSided) {
             Object.material = new MeshPhongMaterial({color: Color})
             Object.castShadow = true
             Object.receiveShadow = true
-            Object.position.z = LeftSided && -Side.length-1.7 || Side.length+1.7
             WingObject = Object
         }
     })
-    Side.push(WingGLTF.scene)
-    return {
-        GLTF: WingGLTF,
-        Object: WingObject
-    }
+    Side.push(WingObject)
+    return WingObject
 }
 
 async function CreateRing(Color = 0xffffff) {
-    const RingGLTF = await GLTF('/public/3D/Ring.gltf')
+    const RingGLTF = await GLTF('/3D/Ring.gltf')
     let RingObject = null
 
     RingGLTF.scene.traverse((Object) => {
@@ -75,9 +48,28 @@ async function CreateRing(Color = 0xffffff) {
             RingObject = Object
         }
     })
-    WingAssets.Ring = RingGLTF.scene
-    return {
-        GLTF: RingGLTF,
-        Object: RingObject
+    return RingObject
+}
+
+export class Wings {
+    async GlitcherWings() {
+        WingAssets.Ring = await CreateRing(0xff0000)
+        WingAssets.Left.push(await CreateRing(0xff0000, true))
+        WingAssets.Left.push(await CreateRing(0xff0000, true))
+        WingAssets.Left.push(await CreateRing(0xff0000, true))
+        WingAssets.Right.push(await CreateRing(0xff0000))
+        WingAssets.Right.push(await CreateRing(0xff0000))
+        WingAssets.Right.push(await CreateRing(0xff0000))
+    
+       // WingAssets.Ring.rotation.x = Math.PI/2
+        WingAssets.Ring.scale.set(3,3,3)
+        WingAssets.Union.RingC0 = new Union(CharacterMesh.Torso, WingAssets.Ring)
+        
+        return [WingAssets.Ring, ...WingAssets.Left, ...WingAssets.Right]
+    }
+
+    Wing_Unions_update() {
+        if (WingAssets.Union.RingC0 != null)
+           WingAssets.Union.RingC0.C0()
     }
 }
