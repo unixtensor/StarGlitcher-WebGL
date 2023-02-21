3ee4f2012a7438945d82ea5330180853630c5a34
interpreterK <rhpidfyre@tuta.io>
2023-02-21 12:35:03 -0500

    Must use arguments comment

--- src/modules/three/Animations.js
+++ src/modules/three/Animations.js
@@ -6,7 +6,7 @@ const rad = (x) => x*Math.PI/180
 const vlerp = (Vector, Alpha) => new Vector3().lerp(Vector, Alpha)
 
 export const Animations = {
-	Idle: (delta) => {
+	Idle: (delta) => { //Use Arguments!
 		Joints.RJ.C0(vlerp(new Vector3(3*Math.cos(delta/500),3*Math.sin(delta/500),0), .5), new Euler(rad(200*Math.cos(delta/1e3)),0,0))
 		
 	}
